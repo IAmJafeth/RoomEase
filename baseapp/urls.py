da17df209eadadbe5ea4862b240c37c9adcdbe8b
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2023-10-31 22:29:59 -0600

    Working on User Login Frontend
    Pending to complete user authentication

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -22,5 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('roomreservation.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
     path('account/',include('account.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- roomreservation/views.py
+++ roomreservation/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .models import Room, Reservation
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'roomreservation/home.html')
+    return render(request, 'roomreservation/home.html',
+                {
+                    'page' : 'home',
+                })
 
 def list_rooms(request):
-    return render(request, 'roomreservation/rooms.html')
+    rooms = Room.objects.all().filter(active=True)
+    return render(request, 'roomreservation/rooms.html',
+                {
+                    'page' : 'rooms',
+                    'rooms': rooms,
+                })
+
+
+
