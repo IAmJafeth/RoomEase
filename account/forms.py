9a72f74372fb6bc32a74a8ebdf98e999491ef86e
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2023-11-02 19:16:57 -0600

    Added Pillow to requirements / started working on user sign up

--- account/forms.py
+++ account/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Account
+
+
+def AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'country', 'image']
